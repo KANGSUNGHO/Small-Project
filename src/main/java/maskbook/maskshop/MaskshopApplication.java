ffaa5667dd0ee7608800d3f8642cca4e0a00a0ff
KANGSUNGHO <kangsho15@gc.gachon.ac.kr>
2020-10-03 14:27:48 +0900

    프로젝트 환결설정

---
+++ src/main/java/maskbook/maskshop/MaskshopApplication.java
@@ -0,0 +1,13 @@
+package maskbook.maskshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskshopApplication.class, args);
+	}
+
+}
